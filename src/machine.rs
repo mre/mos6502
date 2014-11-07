f3160e1bcc7fd8a431c01e7de367b359077c6a67
Andrew Keeton <andrewrkeeton@gmail.com>
2014-11-06 20:02:40 -0500

    Add branch_if_positive().

--- src/machine.rs
+++ src/machine.rs
@@ -583,6 +583,12 @@ impl Machine {
         }
     }
 
+    fn branch_if_positive(&mut self, addr: Address) {
+        if !self.registers.status.contains(PS_NEGATIVE) {
+            self.registers.program_counter = addr;
+        }
+    }
+
     fn push_on_stack(&mut self, val: u8) {
         let addr = self.registers.stack_pointer.to_address();
         self.memory.set_byte(addr, val);
