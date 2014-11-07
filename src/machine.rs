ecb3f0745952819d7b0351821496be508180c07c
Andrew Keeton <andrewrkeeton@gmail.com>
2014-11-06 19:30:05 -0500

    Add branch_if_carry_clear_test().

--- src/machine.rs
+++ src/machine.rs
@@ -861,6 +861,19 @@ fn jump_test() {
 }
 
 #[test]
+fn branch_if_carry_clear_test() {
+    let mut machine = Machine::new();
+
+    machine.execute_instruction((instruction::SEC, instruction::UseImplied));
+    machine.branch_if_carry_clear(Address(0xABCD));
+    assert_eq!(machine.registers.program_counter, Address(0));
+
+    machine.execute_instruction((instruction::CLC, instruction::UseImplied));
+    machine.branch_if_carry_clear(Address(0xABCD));
+    assert_eq!(machine.registers.program_counter, Address(0xABCD));
+}
+
+#[test]
 fn branch_if_minus_test() {
     {
         let mut machine      = Machine::new();
